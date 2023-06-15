4c9990fbbce108a31530f831b4e065b156b00de8
RikuDev <riku@rikudev.com>
2023-06-15 19:10:50 +0200

    search icon fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { API_KEY } from './Api';
-import spinner from './assets/Spinner.svg'
-import search from './assets/Search.svg'
+import spinner from './assets/Spinner.svg';
+import search from './assets/Search.svg';
 import './App.css';
 
 function App() {
@@ -14,74 +14,82 @@ function App() {
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}&units=metric`
       )
-      .then((response) => {
-        if (!response.ok) {
-          setLocation('')
-          setWeatherData(null)
-          throw new Error('City not found')
-        }
-        setError('')
-        return response.json();
-      })
+        .then((response) => {
+          if (!response.ok) {
+            setLocation('');
+            setWeatherData(null);
+            throw new Error('City not found');
+          }
+          setError('');
+          return response.json();
+        })
         .then((data) => setWeatherData(data))
-        .catch((error) => setError(error.message))
+        .catch((error) => setError(error.message));
     }
-  }
+  };
 
   return (
-    <div className='parent'>
-    <div className='search'>
-      <input
-        type="text"
-        value={location}
-        onChange={(event) => {
-          setLocation(event.target.value)
-        }}
-        placeholder="Write your city name"
-        onKeyUp={(event) => {
-          if (event.key === 'Enter') {
-            fetchData()
-          }
-        }}
-      />
-      <img src={search} alt='search' onClick={fetchData}></img>
+    <div className="parent">
+      <div className="search">
+        <label>
+          <input
+            type="text"
+            value={location}
+            onChange={(event) => {
+              setLocation(event.target.value);
+            }}
+            placeholder="Write your city name"
+            onKeyUp={(event) => {
+              if (event.key === 'Enter') {
+                fetchData();
+              }
+            }}
+          />
+          <div className="searchIcon">
+            <img src={search} alt="search" onClick={fetchData} />
+          </div>
+        </label>
       </div>
       {weatherData && (
-        <div className='weatherParent'>
-          <div className='topInfoParent'>
-          <div className='cityName'>
-            <span>{weatherData.name}</span>
-          </div>
-            <div className='temperature'>
-              <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="icon" />
+        <div className="weatherParent">
+          <div className="topInfoParent">
+            <div className="cityName">
+              <span>{weatherData.name}</span>
+            </div>
+            <div className="temperature">
+              <img
+                src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+                alt="icon"
+              />
               <span>{Math.round(weatherData.main.temp)}º</span>
             </div>
-            <div className='description'>
+            <div className="description">
               <span>{weatherData.weather[0].description}</span>
             </div>
           </div>
-          
-            <div className='separator'></div>
-            
-            <div className='otherData'>
-              <div className='feelMax'>
-                <span>Feels like: {Math.round(weatherData.main.feels_like)}º</span>
-                <span>Max temp: {Math.round(weatherData.main.temp_max)}º</span>
-              </div>
-              <div className='humWind'>
-                <span>Humidity: {weatherData.main.humidity}%</span>
-                <span>Wind: {Math.round(weatherData.wind.speed *3.6)}km/h</span>
-              </div>
+
+          <div className="separator"></div>
+
+          <div className="otherData">
+            <div className="feelMax">
+              <span>Feels like: {Math.round(weatherData.main.feels_like)}º</span>
+              <span>Max temp: {Math.round(weatherData.main.temp_max)}º</span>
             </div>
+            <div className="humWind">
+              <span>Humidity: {weatherData.main.humidity}%</span>
+              <span>Wind: {Math.round(weatherData.wind.speed * 3.6)}km/h</span>
+            </div>
+          </div>
         </div>
       )}
       {error && (
-        <div className='error'>
-          <img src={spinner} alt='spinner'></img>
+        <div className="error">
+          <img src={spinner} alt="spinner" />
           <p>{error}</p>
-        </div>)}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
