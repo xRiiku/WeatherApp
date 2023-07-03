5836b0791c68969aa8c49cd9f0e7df245cdbc0a4
RikuDev <riku@rikudev.com>
2023-07-03 19:03:07 +0200

    cambio de dominio

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://rikudev.com/WeatherApp', // Configura la ruta base de tu aplicación
+  base: 'https://weather.rikudev.com', // Configura la ruta base de tu aplicación
   build: {
     outDir: 'dist', // Configura la carpeta de salida para los archivos generados por el comando npm run build
   },
